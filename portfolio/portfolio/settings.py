5ccdb1f3c45e0454bb2135cf306951a61fec1b57
Jose Mendoza <sagaka1g@gmail.com>
2021-08-23 22:08:14 +0100

    Updated setting files

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'y3a^be%tx=mc*fsk+qs!_qfnb2y#35#isj4c@y$(_92pg9h^^!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sagaka.pythonanywhere.com']
 
 
 # Application definition
